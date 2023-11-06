3b86fb130de78bcf7f48d777fa4dd8df1dd14b85
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-06 12:07:19 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var button = document.getElementById('entrybutton');
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Leen Mobarki: This is a test " + textbox.value);
+    alert("Leen Mobarki: " + textbox.value);
   }
   button.addEventListener('click', showMeText);
   }
