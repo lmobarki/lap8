7245b44ddf8ec541410a6ab36333c3942057259a
lmobarki <143443056+lmobarki@users.noreply.github.com>
2023-11-06 12:10:12 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-var button = document.getElementById('entrybutton');
+  var button = document.getElementById('entrybutton');
 
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Leen Mobarki: " + textbox.value);
-  }
-  button.addEventListener('click', showMeText);
+    alert("Daniel Krieglstein: " + textbox.value);
   }
 
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
